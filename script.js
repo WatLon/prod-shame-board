ab44105bdb05ab6a2af837c411210aace497b6a9
Kirill Udaltsov <commits@udaltsovk.ru>
2026-04-08 22:21:48 +0300

    fix: use imagemagick to convert avatars
    I love Telegram devs soooooo muuuuuuch

--- script.js
+++ script.js
@@ -58,7 +58,7 @@
 
     return `
             <div class="entry-avatar" style="background:${color}">
-                <img src="avatars/${clean}.png"
+                <img src="avatars/${clean}.webp"
                      alt="" loading="lazy"
                      onerror="this.remove()">
                 ${letter}
